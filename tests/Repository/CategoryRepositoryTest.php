c48b2ad4f671da141e52d8d6ce5881b98bdfe100
Venom2879 <jerome.sauvan@gmail.com>
2026-02-17 16:11:19 +0100

    test CategoryRepository

---
+++ tests/Repository/CategoryRepositoryTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Tests\Repository;
+
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class CategoryRepositoryTest extends KernelTestCase
+{
+    public function testFindAllCategory()
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+
+        $categories = count($container->get(CategoryRepository::class)->findAll([]));
+        $this->assertEquals(6, $categories);
+    }
+
+    public function testFindOneByTitleCategoryById():void {
+
+        self::bootKernel();
+        $container = static::getContainer();
+
+        $category[] = $container->get(CategoryRepository::class)->findOneBy(['title' => 'Chaussure']);
+$this->assertEquals(1, count($category));
+    }
+
+}
